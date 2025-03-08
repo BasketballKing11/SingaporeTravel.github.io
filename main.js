55cdd566e994d47ab860f67b2c5d4458ce5191d9
BasketballKing11 <tt20winn@gmail.com>
2025-03-07 21:23:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//This code makes it so text on the page appears when within a certain range of the viewable screen
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
